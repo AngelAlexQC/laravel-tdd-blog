78007180b226c6225d51a9f8e94abc9a3e6517f7
Ángel Quiroz <guirudj007@gmailc.om>
2021-08-12 13:56:03 -0500

    Jetstream & (Posts & Users Tests)

---
+++ database/migrations/2020_05_21_200000_create_team_user_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\Team;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeamUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('team_user', function (Blueprint $table) {
+            $table->id();
+            $table->string('role')->nullable();
+            $table->timestamps();
+            $table->foreignIdFor(Team::class, 'team_id')->references('id')->on('teams');
+            $table->foreignIdFor(User::class, 'user_id')->references('id')->on('users');
+            $table->unique(['team_id', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('team_user');
+    }
+}
